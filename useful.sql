b1015b8e2daebbae8d8b04d5b12221aafe201e34
Codeman Company <gustavo@codeman.company>
2019-02-27 20:24:57 -0600

    Progress Linux

--- useful.sql
+++ useful.sql
@@ -10,3 +10,6 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 
 -- Create user
 CREATE USER 'man_master'@'localhost' IDENTIFIED BY 'password';
+
+-- Import data
+mysql -u username -p database_name < file.sql
